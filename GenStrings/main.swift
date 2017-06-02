9118f31800489a5406ab5639f99afb1e443fbf62
vikas goyal <vikas.goyal@webvirtue.com>
2017-06-02 15:02:07 +0530

    Initial Commit

---
+++ GenStrings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenStrings
+//
+//  Created by Vikas Goyal on 02/06/17.
+//  Copyright © 2017 Vikas Goyal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
